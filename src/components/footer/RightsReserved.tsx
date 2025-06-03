53d76e39a2061af717f56c18f2d5be7370b1279f
DevPBDias <devpbdias@gmail.com>
2025-06-03 16:35:44 -0300

    fix(footer): change copy sign to avoid problem in vercel deploy

--- src/components/footer/RightsReserved.tsx
+++ src/components/footer/RightsReserved.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const RightsReserved = () => {
   return (
     <div className="text-center text-sm/[130.8%] font-medium tracking-[-2%] text-[var(--text-color)] py-8 px-[60px] w-full border-t border-[var(--footer-border-color)]">
-      © {new Date().getFullYear()} Tirar Visto - Todos os Direitos Reservados
+      `&apos;` {new Date().getFullYear()} Tirar Visto - Todos os Direitos
+      Reservados
     </div>
   );
 };
