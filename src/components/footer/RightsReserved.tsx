ac6ec17fef9a511fb620fdaf4586d174aa4213fd
DevPBDias <devpbdias@gmail.com>
2025-06-03 16:47:37 -0300

    fix: fix react/no-unescaped-entitie' of an ESLint rule

--- src/components/footer/RightsReserved.tsx
+++ src/components/footer/RightsReserved.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const RightsReserved = () => {
   return (
     <div className="text-center text-sm/[130.8%] font-medium tracking-[-2%] text-[var(--text-color)] py-8 px-[60px] w-full border-t border-[var(--footer-border-color)]">
-      `&apos;` {new Date().getFullYear()} Tirar Visto - Todos os Direitos
-      Reservados
+      © {new Date().getFullYear()} Tirar Visto - Todos os Direitos Reservados
     </div>
   );
 };
--- src/components/why/HowItWorksContent.tsx
+++ src/components/why/HowItWorksContent.tsx
@@ -15,11 +15,11 @@ function HowItWorksContent() {
           </h1>
           <p className="text-[var(--text-secondary-color)] font-normal text-base/[130.8%] tracking-[-2%] w-2/3">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged.
+            industry. Lorem Ipsum has been the industry&apos;s standard dummy
+            text ever since the 1500s, when an unknown printer took a galley of
+            type and scrambled it to make a type specimen book. It has survived
+            not only five centuries, but also the leap into electronic
+            typesetting, remaining essentially unchanged.
           </p>
         </div>
       </div>
