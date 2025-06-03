2767ef8d9f381adfa9d9224daee6f4efa420dde9
DevPBDias <devpbdias@gmail.com>
2025-06-02 22:58:17 -0300

    feat: add constants values for mock hero content

---
+++ src/constants/hero-stats.ts
@@ -0,0 +1,10 @@
+export type Stat = {
+  label: string;
+  value: number;
+};
+
+export const stats: Stat[] = [
+  { label: "Vistos Tirados", value: 509 },
+  { label: "Passaportes Tirados", value: 602 },
+  { label: "Famílias Felizes", value: 634 },
+];
---
+++ src/constants/navLinks.ts
@@ -0,0 +1,23 @@
+export interface INavLinks {
+  name: string;
+  link: string;
+}
+
+export const navLinks: INavLinks[] = [
+  {
+    name: "Home",
+    link: "#home",
+  },
+  {
+    name: "Quem Somos",
+    link: "#about",
+  },
+  {
+    name: "Serviços",
+    link: "#services",
+  },
+  {
+    name: "Contato",
+    link: "#contact",
+  },
+];
